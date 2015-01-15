a0a656c0acde08f3b9bc24918998b593cf8782f6
pv-atmos <mjucker@mjj.local>
2015-01-15 12:18:06 -0500

    Bugfix: Add to existing file
    If WriteGenericNc is invoked with a filename that already exists, the
    variable is now added to the existing file.
    The function checks for already existing dimensions that might be the
    same as in the new variable. If that’s the case, the variable is
    attributed to the existing dimensions. If not, new dimensions are added
    to the file.

--- tools.py
+++ tools.py
@@ -10,6 +10,54 @@
 
 ############################################################################################
 
+## check if dimension already exists, and if so, create new dimension name
+#
+def MakeNewDimensionName(file,dimName):
+    """Check if desired dimension name already exists.
+       If so, add number until dimension name is unique
+
+       INPUTS:
+       file:    netCDF4 dataset
+       dimName: desired dimension name
+       OUPUT:
+       newName: new unique dimension name
+    """
+    if dimName not in file.dimensions:
+        newName = dimName
+    else:
+        newName = dimName
+        i = 0
+        while newName in file.dimensions:
+            i += 1
+            newName = dimName + str(i)
+        return newName
+
+## check if the given dimension already exists in a file. 
+#
+def CheckDimension(file,dimName,dimVal):
+    """Check if desired dimension already exists in a file.
+       If not, returns new dimension name.
+
+       INPUTS:
+       file:    netCDF4 dataset
+       dimName: dimension name to be checked
+       dimVal:  value of dimension to be checked
+       OUTPUT:
+       'exists':   dimension already exists and is identical
+       'someName': dimension does not exist - returns unique new name
+    """
+    from numpy import allclose
+    if dimName in file.dimensions:
+        if dimName not in file.variables:
+            raise Exception('Dimension '+dimName+' has no variable assiciated with it')
+        dimExist = file.variables[dimName][:]
+        if  allclose(dimExist,dimVal):
+            return 'exists'
+        else:
+            return MakeNewDimensionName(file,dimName)
+    else:
+        return dimName
+
 ## create a generic netCDF file that can be read with pv_atmos
 #
 def WriteGenericNc(x, y, z, t, data, varName, outFileName='ncGeneric.nc'):
@@ -32,41 +80,60 @@ def WriteGenericNc(x, y, z, t, data, varName, outFileName='ncGeneric.nc'):
     dims = ()
     if op.isfile(outFileName):
         mode = 'r+'
+        text = 'Added to'
     else:
         mode = 'w'
+        text = 'Created'
     outFile = nc.Dataset(outFileName, mode, format='NETCDF3_64BIT')
+    # check if dimensions x,y,z already exist, and add them if not
     if len(x) > 0:
-        xD = outFile.createDimension('x',len(x))
-        xV = outFile.createVariable('x','f4', ('x',))
-        xV[:] = x
-        xV.setncattr('long_name','X axis')
-        xV.setncattr('cartesian_axis','X')
-        dims = ('x',) + dims
+        checkDim = CheckDimension(outFile,'x',x)
+        if checkDim == 'exists':
+            checkDim = 'x'
+        else:
+            xD = outFile.createDimension(checkDim,len(x))
+            xV = outFile.createVariable(checkDim,'f4', (checkDim,))
+            xV[:] = x
+            xV.setncattr('long_name','X axis')
+            xV.setncattr('cartesian_axis','X')
+        dims = (checkDim,) + dims
     if len(y) > 0:
-        yD = outFile.createDimension('y',len(y))
-        yV = outFile.createVariable('y','f4', ('y',))
-        yV[:] = y
-        yV.setncattr('long_name','Y axis')
-        yV.setncattr('cartesian_axis','Y')
-        dims = ('y',) + dims
+        checkDim = CheckDimension(outFile,'y',y)
+        if checkDim == 'exists':
+            checkDim = 'y'
+        else:
+            yD = outFile.createDimension(checkDim,len(y))
+            yV = outFile.createVariable(checkDim,'f4', (checkDim,))
+            yV[:] = y
+            yV.setncattr('long_name','Y axis')
+            yV.setncattr('cartesian_axis','Y')
+        dims = (checkDim,) + dims
     if len(z) > 0:
-        zD = outFile.createDimension('z',len(z))
-        zV = outFile.createVariable('z','f4', ('z',))
-        zV[:] = z
-        zV.setncattr('long_name','Z axis')
-        zV.setncattr('cartesian_axis','Z')
-        dims = ('z',) + dims
+        checkDim = CheckDimension(outFile,'z',z)
+        if checkDim == 'exists':
+            checkDim = 'z'
+        else:
+            zD = outFile.createDimension(checkDim,len(z))
+            zV = outFile.createVariable(checkDim,'f4', (checkDim,))
+            zV[:] = z
+            zV.setncattr('long_name','Z axis')
+            zV.setncattr('cartesian_axis','Z')
+        dims = (checkDim,) + dims
     if len(t) > 0:
-        tD = outFile.createDimension('time',len(t))
-        tV = outFile.createVariable('time','f4', ('time',))
-        tV[:] = t
-        tV.setncattr('long_name','time')
-        tV.setncattr('cartesian_axis','T')
-        tV.setncattr('units','time since 0000-00-00 00:00:00')
-        dims = ('time',) + dims
+        checkDim = CheckDimension(outFile,'time',t)
+        if checkDim == 'exists':
+            checkDim = 'time'
+        else:
+            tD = outFile.createDimension(checkDim,len(t))
+            tV = outFile.createVariable(checkDim,'f4', (checkDim,))
+            tV[:] = t
+            tV.setncattr('long_name','time')
+            tV.setncattr('cartesian_axis','T')
+            tV.setncattr('units','time since 0000-00-00 00:00:00')
+        dims = (checkDim,) + dims
     vOut = outFile.createVariable(varName,'f4',dims)
     vOut[:] = data
     outFile.close()
-    print 'Done, created file'+outFileName
+    print text+' file '+outFileName
 
 
