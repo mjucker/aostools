7b1d55cee365ceffebe136fad7f44f635340841e
Martin Jucker <coding@martinjucker.com>
2017-01-13 15:54:26 +1100

    Added setup.py
    For future packaging, added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='aostools',
+      version='1.2',
+      description='Helper functions for postprocessing and analysis of netCDF data',
+      long_description='',
+      author='Martin Jucker',
+      author_email='coding@martinjucker.com',
+      license='MIT',
+      url='https://github.com/mjucker/aostools',
+      package_dir={'aostools': ''},
+      packages=['aostools'],
+      )
