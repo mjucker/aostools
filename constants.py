68319dc9d3586442ff795c916911b8972d41df57
Martin Jucker <coding@martinjucker.com>
2020-06-29 14:05:48 +1000

    Create constants.py
    Constants.py inclues some often used constants. Can and will be expanded in the future.

---
+++ constants.py
@@ -0,0 +1,8 @@
+g = 9.81 #[m/s2 ]
+a0 = 6376.0e3 # [m]
+Omega = 7.292e-5 #[1/s]
+Rd = 287.04 # [J/kg/K]
+Rv = 461.50 # [J/kg/K]
+kappa = 2./7. # []
+cp = Rd/kappa # [J/kg/K]
+sigma = 5.6734e-8 # [W/m2/K4]
