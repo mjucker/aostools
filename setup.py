ed602be3f70c62379029c2f0765bbb2b165c2d7d
Martin Jucker <coding@martinjucker.com>
2022-04-26 18:32:30 +1000

    updated version info

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 	return open(os.path.join(os.path.dirname(__file__),fname)).read()
 
 setup(name='aostools',
-      version='2.3.2',
+      version='2.4',
       description='Helper functions for scientific postprocessing and analysis of netCDF data',
       long_description=read('readme_pypi'),
       keywords='atmospheric oceanic science netcdf analysis tools',
