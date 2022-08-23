c0a62720b7578abb99d7c5b3f5dd1b217a123c42
Martin Jucker <coding@martinjucker.com>
2022-08-23 11:54:22 +1000

    new `DetectEvents()` function detects events of any array reaching a threshold for a given time period.

--- climate.py
+++ climate.py
@@ -1345,7 +1345,7 @@ def ComputeWaveActivityFlux(phi_or_u,phiref_or_v,uref=None,vref=None,lat='infer'
 						 Else, assume S2 = tref.
 						 Note that S2 should be a function of
 						  pressure only (see Vallis 2017, Eq 5.127)
-        is_anomaly : if True, phi_or_u and phiref_or_v is anomaly. If False, they are full fields. [False]
+                is_anomaly : if True, phi_or_u and phiref_or_v is anomaly. If False, they are full fields. [False]
 		qg	       : use QG streamfunction (phi-phiref)/f? Otherwise, use windspharm.streamfunction.
 					   Note that if qg=False, phi_or_u = u and phiref_or_v = v to compute the streamfunction.
 		use_windspharm: use windspharm package to compute streamfunction. Has no effect if qg=True.
@@ -2998,4 +2998,83 @@ def Regress(x,y,dim):
 	'''
 	from xarray import cov
 	return cov(x,y,dim)/y.std(dim)
-	
+
+#######################################################
+def DetectEvents(ds,thresh,sep=20,period=1,time='time',kind='auto'):
+    '''
+    Find events below (kind='min') or above (kind='max') a given threshold for a certain period of time. Events are merged into the earlier if less than a given separation time between them.
+
+    INPUTS:
+        ds:     xarray.dataarray used to detect events
+        thresh: threshold to define events
+        sep:    minimum separation of individual events (from end to start) [number of timesteps]
+        period: minimum duration of each event, i.e. #time steps above threshold
+        time:   name of time dimension
+        kind:   find minimum if 'min', maximum if 'max'.
+                if 'auto': minimum if thresh <= 0, maximum elsewhise
+
+    OUTPUTS:
+        stats: xarray.Dataset of 
+          onset_date:    onset dates of each event
+          end_date:      end date of each event
+          duration:      duration of each event, i.e. end_date - onset_date
+          extreme_value: most extreme value during each event period.
+    '''
+    import xarray as xr
+    from datetime import timedelta
+    ds = ds.rename({time:'time'})
+    da = ds.rolling(time=period)
+    if kind == 'auto':
+        if thresh <= 0:
+            kind = 'min'
+        else:
+            kind = 'max'
+    if kind.lower() == 'max':
+        da = da.min()
+        times = da > thresh
+    elif kind.lower() == 'min':
+        da = da.max()
+        times = da < thresh
+    # in case no event has been detected
+    if np.sum(times) == 0:
+        return None
+    event_all = da.isel(time=times)
+    timestep = ds.time[1] - ds.time[0]
+    e = 0
+    event_id = [e]
+    tm1 = event_all.time[0]
+    for t in event_all.time[1:]:
+        delta_t = t - tm1
+        if delta_t > sep*timestep:
+            e += 1
+        tm1 = t
+        event_id.append(e)
+    ex = xr.DataArray(event_id,coords=[event_all.time],name='event_id')
+    unique_events = np.unique(ex)
+    ts_secs = timedelta(seconds=timestep.values/np.timedelta64(1,'s'))
+    duration = []
+    extreme  = []
+    start_dates = []
+    end_dates = []
+    for ue in unique_events:
+        filtr = ex == ue
+        prev_date = ex.isel(time=filtr).time[0].values - period*ts_secs
+        # this is a workaround to avoid having objects or arrays of arrays
+        end_date  = ex.isel(time=filtr).time[-1].values+ 0*ts_secs
+        start_dates.append(prev_date)
+        end_dates.append(end_date)
+        duration.append(period+sum(filtr.values)-1)
+        if kind == 'min':
+            extreme.append(ds.sel(time=slice(prev_date,end_date)).min().values)
+        elif kind == 'max':
+            extreme.append(ds.sel(time=slice(prev_date,end_date)).max().values)
+    unicord = [('event',unique_events)]
+    onx  = xr.DataArray(start_dates,coords=unicord,name='onset_date')
+    edx  = xr.DataArray(end_dates,coords=unicord,name='end_date')
+    durx = xr.DataArray(duration,coords=unicord,name='duration')
+    extx = xr.DataArray(extreme,coords=unicord,name='extreme_value')
+    outx = xr.merge([durx,extx,onx,edx])
+    outx.attrs['variable'] = ds.name
+    options = {'max':'above','min':'below'}
+    outx.attrs['method'] = 'individual {0}-day periods of {4} {1} {2}. Events are considered the same if spaced by less than {3} days'.format(period,options[kind],thresh,sep,ds.name)
+    return outx 
